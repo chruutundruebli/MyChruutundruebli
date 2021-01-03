064dadfa8c5f49a15974c08363434d08071f9071
Claudia Spichtig <loa.clau@gmail.com>
2021-01-03 18:06:27 +0100

    commit

--- chruutundruebli/urls.py
+++ chruutundruebli/urls.py
@@ -12,7 +12,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url, path
+from django.conf.urls import include, url
+from django.urls import path
 from django.contrib import admin
 import juntagrico
 import chruutundruebli
