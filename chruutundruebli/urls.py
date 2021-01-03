1ef82635f1c660e369b147c2344d538b02802cad
Claudia Spichtig <loa.clau@gmail.com>
2021-01-03 18:05:03 +0100

    commit

--- chruutundruebli/urls.py
+++ chruutundruebli/urls.py
@@ -12,13 +12,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, path
 from django.contrib import admin
 import juntagrico
+import chruutundruebli
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('juntagrico.urls')),
     url(r'^$', juntagrico.views.home),
     url(r'^impersonate/', include('impersonate.urls')),
+
+    #export
+    path('my/export/subscriptions', chruutundruebli.excel_export_subscriptions, name='export-subscriptions')
 ]
