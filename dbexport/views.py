c2880f68c826df7259a14f104f9aa315ca508345
Samuele Bischof <samuele.bischof@ti8m.ch>
2021-10-02 20:37:36 +0200

    Add DB dump

--- dbexport/views.py
+++ dbexport/views.py
@@ -1,9 +1,11 @@
+import os
 from django.contrib.auth.decorators import permission_required
-from django.http.response import JsonResponse
-
-from chruutundruebli.settings import DATABASES
+from django.core.management import call_command
+from django.views.static import serve
 
 @permission_required('juntagrico.can_view_exports')
 def db_export(request):
-  
-  return JsonResponse(DATABASES)
+  filepath = './db.json'
+  with open(filepath, 'w') as f:
+    call_command('dumpdata', stdout=f)
+  return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
