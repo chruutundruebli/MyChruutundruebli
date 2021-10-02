7fb4f2cbea846ea518a4fc53e8b70f87946c4f57
Samuele Bischof <samuele@bischof.cloud>
2021-10-02 18:01:02 +0200

    Update views.py
    Check for files in base folder

--- dbexport/views.py
+++ dbexport/views.py
@@ -1,8 +1,11 @@
 import os
 from django.contrib.auth.decorators import permission_required
-from django.views.static import serve
+from django.http.response import HttpResponse
+# from django.views.static import serve
 
 @permission_required('juntagrico.can_view_exports')
 def db_export(request):
-  filepath = './chruutundruebli.db'
-  return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
+  # filepath = './chruutundruebli.db'
+  arr = os.listdir()
+  # return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
+  return HttpResponse(", ".join(arr))
