63f895a51ea477ecc407accc360a99dc71c6b023
Samuele Bischof <samuele.bischof@ti8m.ch>
2021-10-03 17:11:41 +0200

    Add env variable for docker username

--- docker/resources/startup.sh
+++ docker/resources/startup.sh
@@ -4,7 +4,7 @@
 ./manage.py collectstatic --noinput
 
 if [ -e first_startup.txt ]; then
-  ./manage.py createsuperuser --noinput --username superadmin --email superadmin@localhost
+  ./manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email superadmin@localhost
   ./manage.py create_member_for_superusers
   ./manage.py generate_testdata_advanced
   rm -rf first_startup.txt
