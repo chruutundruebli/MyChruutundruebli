4a96b51ef223c120f484f5b5efb8cf68cc56e953
Samuele Bischof <samuele.bischof@ti8m.ch>
2021-10-03 10:33:50 +0200

    Revert "Use streaming for db-export"
    This reverts commit 8b3618ad367dc2b522e4e2027fb40b17d7a35673.

--- dbexport/views.py
+++ dbexport/views.py
@@ -1,10 +1,9 @@
 import os
-import mimetypes
 import subprocess
 from threading import Thread
 from django.contrib.auth.decorators import permission_required
-from django.http.response import HttpResponseRedirect, StreamingHttpResponse
-from wsgiref.util import FileWrapper
+from django.http.response import HttpResponseRedirect
+from django.views.static import serve
 
 def generate_db_export():
   process = subprocess.Popen(['./manage.py', 'dumpdata', '-o', 'db.json'],
@@ -20,11 +19,5 @@ def db_export_generate(request):
 
 @permission_required('juntagrico.can_view_exports')
 def db_export(request):
-  the_file = './db.json'
-  filename = os.path.basename(the_file)
-  chunk_size = 8192
-  response = StreamingHttpResponse(FileWrapper(open(the_file, 'rb'), chunk_size),
-                          content_type=mimetypes.guess_type(the_file)[0])
-  response['Content-Length'] = os.path.getsize(the_file)    
-  response['Content-Disposition'] = "attachment; filename=%s" % filename
-  return response
+  filepath = './db.json'
+  return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
