a3ad231f99c10aa0659de4f60170a4d0f9eb58cd
Samuele Bischof <samuele.bischof@ti8m.ch>
2023-07-16 19:30:32 +0200

    Add pg to installed apps

--- chruutundruebli/settings.py
+++ chruutundruebli/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'juntagrico',
+    'juntagrico_pg',
     'impersonate',
     'crispy_forms',
     'chruutundruebli',
